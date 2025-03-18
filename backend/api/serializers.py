9ea1e0c31a58095e0c5ef8d8ccdf104976a4c08c
Brayan <brayanmserpa@gmail.com>
2025-03-18 15:38:36 -0300

    criada views e urls

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -1,4 +1,4 @@
-from django.contrib.auth.models import UserAttributeSimilarityValidator
+from django.contrib.auth.models import User
 from rest_framework import serializers
 
 class UserSerializer(serializers.ModelSerializer):
@@ -7,7 +7,7 @@ class UserSerializer(serializers.ModelSerializer):
         fields = ["id", "username", "password"]
         extra_kwargs = {"passowrd": {"write_only": True}}
 
-        def create(self, validate_data):
-            user = User.objects.create_user(**validated_data)
-            return user
+    def create(self, validate_data):
+        user = User.objects.create_user(**validated_data)
+        return user
             
